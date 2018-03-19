137dee1a40dc6d6618543f5ee2e229ada33b9ffb
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-19 18:40:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,10 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash.each do |season, holiday_hash|
+    holiday_hash.values
+  end
+      
 end
 
 def all_supplies_in_holidays(holiday_hash)
