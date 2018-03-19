99c6077b769313d12a0faab7de15e0f48d7cfada
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-19 18:40:34 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash.each do |season, holiday_hash|
     holiday_hash.values
   end
-      
+
 end
 
 def all_supplies_in_holidays(holiday_hash)
