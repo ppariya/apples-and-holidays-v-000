a26fcb32e18efcbd263e4e7b8415319ae4d1a54a
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-19 18:25:40 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,6 +42,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_supplies.each do |seasons, value|
     if seasons == season
       value[:holiday_name] = supply_array
+    end
 end
 
 def all_winter_holiday_supplies(holiday_hash)
