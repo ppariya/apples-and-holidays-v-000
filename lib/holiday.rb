f10cdba56a015c76949245635943c2e953de56a8
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-19 18:45:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday_value|
-    holiday_value.values.flatten
+    holiday_value.values.flatten.list
   end
 end
 
